4db63e98ec9a13d29ec7e8f5c43df4a6016b2f1d
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2026-08-01 15:21:44 -0300

    fix: update dependencies to resolve vulnerabilities

--- node/index.js
+++ node/index.js
@@ -1,23 +1,23 @@
 import cluster from "node:cluster";
-import Fastify from "fastify";
 import os from "node:os";
+import Fastify from "fastify";
 
 const numClusterWorkers = os.cpus().length;
 console.log("Número de núcleos: ", numClusterWorkers);
 
 if (cluster.isPrimary) {
-  for (let i = 0; i < numClusterWorkers; i++) {
-    cluster.fork();
-  }
+	for (let i = 0; i < numClusterWorkers; i++) {
+		cluster.fork();
+	}
 
-  cluster.on("exit", (worker) =>
-    console.log(`worker ${worker.process.pid} died`)
-  );
+	cluster.on("exit", (worker) =>
+		console.log(`worker ${worker.process.pid} died`),
+	);
 } else {
-  const fastify = Fastify({ logger: false });
-  fastify.get("/", () => {
-    return "Hello world!";
-  });
+	const fastify = Fastify({ logger: false });
+	fastify.get("/", () => {
+		return "Hello world!";
+	});
 
-  fastify.listen({ port: 3000 });
+	fastify.listen({ port: 3000 });
 }
